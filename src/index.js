21fdd164869499a7a78656301180c9cf61d20a9c
Anastasiya Ruban <nastja21.91@mail.ru>
2023-02-19 18:23:36 +0300

    add part of PP

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
-import './styles/main.scss';
-
 import Swiper, { Navigation, Pagination } from 'swiper';
-// import Swiper and modules styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import './styles/main.scss';
 
-// init Swiper:
 const swiper = new Swiper('.corporate-life_swiper', {
-  // configure Swiper to use modules
   modules: [Navigation, Pagination],
+  loop: true,
   pagination: {
     el: '.swiper-pagination',
-    type: 'progressbar',
+    type: 'bullets',
+    clickable: true,
   },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 });
-console.log(42);
+
+const bullets = document.querySelectorAll('.swiper-pagination-bullet');
+console.log(bullets.length);
+
+bullets.forEach((bullet) => {
+  const parentWidth = bullet.parentNode.getBoundingClientRect().width;
+  bullet.style.width = `${
+    (parentWidth - (bullets.length - 1) * 10) / bullets.length
+  }px`;
+});
