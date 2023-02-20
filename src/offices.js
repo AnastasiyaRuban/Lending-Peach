80f0d7211877d90cbec8cdcded55fe17354e2641
Anastasiya Ruban <nastja21.91@mail.ru>
2023-02-20 20:48:38 +0300

    add part of sekect

--- src/offices.js
+++ src/offices.js
@@ -1,19 +1,21 @@
 const tabBtns = document.querySelectorAll('.tabs__button');
 const selectBtn = document.querySelector('.select__placeholder');
 const list = document.querySelector('.select__options');
-const map = document.querySelector('.offices__map');
+const mapBlock = document.querySelector('.offices__map');
 const tabs = document.querySelector('.tabs');
+const cityBtn = document.querySelectorAll('.option__city');
+const maps = document.querySelectorAll('.offices__map > img');
+const mapPlug = document.querySelector('.offices__map_plug');
 
 tabBtns.forEach((element) => {
   const tabName = element.getAttribute('data-tab-name');
-  const maps = document.querySelectorAll('.offices__map > img');
 
   element.addEventListener('click', (e) => {
     tabBtns.forEach((btn) => {
       btn.classList.remove('tabs__button_active');
     });
     element.classList.add('tabs__button_active');
-
+    mapPlug.style.display = 'none';
     maps.forEach((map) => {
       if (map.getAttribute('data-path') === tabName) {
         map.classList.add('offices__map-active');
@@ -31,6 +33,25 @@ selectBtn.addEventListener('click', () => {
 
   selectBtn.classList.toggle('active');
   list.classList.toggle('active');
-  map.classList.toggle('open');
+  mapBlock.classList.toggle('open');
   tabs.classList.toggle('open');
+});
+
+cityBtn.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    maps.forEach((map) => {
+      map.classList.remove('offices__map-active');
+      mapPlug.style.display = 'block';
+    });
+    tabBtns.forEach((btn) => {
+      if (!btn.getAttribute('disabled')) {
+        btn.setAttribute('disabled', true);
+      } else btn.removeAttribute('disabled');
+    });
+
+    selectBtn.classList.toggle('active');
+    list.classList.toggle('active');
+    mapBlock.classList.toggle('open');
+    tabs.classList.toggle('open');
+  });
 });
