a3a7753ffeb603ce7e5a7f6e2ec1580fd5224112
Anastasiya Ruban <nastja21.91@mail.ru>
2023-02-22 17:36:55 +0300

    check tabs

--- src/offices.js
+++ src/offices.js
@@ -1,11 +1,25 @@
 const tabBtns = document.querySelectorAll('.tabs__button');
 const selectBtn = document.querySelector('.select__placeholder');
-const list = document.querySelector('.select__options');
+const select = document.querySelector('.select');
 const mapBlock = document.querySelector('.offices__map');
 const tabs = document.querySelector('.tabs');
 const cityBtn = document.querySelectorAll('.option__city');
 const maps = document.querySelectorAll('.offices__map > img');
 const mapPlug = document.querySelector('.offices__map_plug');
+
+function getSelectTopValue() {
+  const parentPaddingTop = getComputedStyle(select.parentNode).paddingTop.slice(
+    0,
+    -2
+  );
+  const hightMenu = getComputedStyle(
+    select.previousElementSibling
+  ).height.slice(0, -2);
+  const gapTop = 20;
+  return Number(parentPaddingTop) + Number(hightMenu) + gapTop + 'px';
+}
+
+select.style.top = getSelectTopValue();
 
 tabBtns.forEach((element) => {
   const tabName = element.getAttribute('data-tab-name');
@@ -32,9 +46,20 @@ selectBtn.addEventListener('click', () => {
   });
 
   selectBtn.classList.toggle('active');
-  list.classList.toggle('active');
+  select.classList.toggle('active');
   mapBlock.classList.toggle('open');
   tabs.classList.toggle('open');
+
+  if (select.style.maxHeight) {
+    select.style.maxHeight = null;
+    select.style.paddingTop = 0;
+    select.style.paddingBottom = 0;
+  } else {
+    select.style.height = select.scrollHeight + 20 + 'px';
+    select.style.maxHeight = select.scrollHeight + 20 + 'px';
+    select.style.paddingBottom = 10 + 'px';
+    select.style.paddingTop = 10 + 'px';
+  }
 });
 
 cityBtn.forEach((btn) => {
@@ -50,7 +75,7 @@ cityBtn.forEach((btn) => {
     });
 
     selectBtn.classList.toggle('active');
-    list.classList.toggle('active');
+    select.classList.toggle('active');
     mapBlock.classList.toggle('open');
     tabs.classList.toggle('open');
   });
