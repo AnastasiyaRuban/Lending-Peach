4be9d55cbdce5a2c0796ca3bf5277cee59259864
Anastasiya Ruban <nastja21.91@mail.ru>
2023-02-23 13:14:32 +0300

    select

--- src/offices.js
+++ src/offices.js
@@ -6,8 +6,29 @@ const tabs = document.querySelector('.tabs');
 const cityBtn = document.querySelectorAll('.option__city');
 const maps = document.querySelectorAll('.offices__map > img');
 const mapPlug = document.querySelector('.offices__map_plug');
+const subSelectBtn = document.querySelectorAll('.option__placeholder');
+const subSelect = document.querySelectorAll('.option__list');
+
+subSelectBtn.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    const select = btn.nextElementSibling;
+
+    btn.classList.toggle('active');
+    if (select.style.maxHeight) {
+      select.style.maxHeight = null;
+      select.style.paddingTop = 0;
+      select.style.paddingBottom = 0;
+    } else {
+      // select.style.height = select.scrollHeight + 20 + 'px';
+      select.style.maxHeight = select.scrollHeight + 20 + 'px';
+      select.style.paddingBottom = 10 + 'px';
+      select.style.paddingTop = 10 + 'px';
+    }
+  });
+});
 
 function getSelectTopValue() {
+  const viewPort = document.documentElement.clientWidth;
   const parentPaddingTop = getComputedStyle(select.parentNode).paddingTop.slice(
     0,
     -2
@@ -15,8 +36,26 @@ function getSelectTopValue() {
   const hightMenu = getComputedStyle(
     select.previousElementSibling
   ).height.slice(0, -2);
+  const hightTabsBlock = getComputedStyle(
+    document.querySelector('.offices__tabs-wrapper')
+  ).height.slice(0, -2);
+
+  const gapInMenuBlock = getComputedStyle(
+    document.querySelector('.offices__menu')
+  ).rowGap.slice(0, -2);
+
   const gapTop = 20;
-  return Number(parentPaddingTop) + Number(hightMenu) + gapTop + 'px';
+  if (viewPort > 425) {
+    console.log(42);
+    return Number(parentPaddingTop) + Number(hightMenu) + gapTop + 'px';
+  }
+  return (
+    Number(parentPaddingTop) +
+    Number(hightMenu) -
+    hightTabsBlock -
+    gapInMenuBlock +
+    'px'
+  );
 }
 
 select.style.top = getSelectTopValue();
@@ -39,6 +78,18 @@ tabBtns.forEach((element) => {
 });
 
 selectBtn.addEventListener('click', () => {
+  subSelect.forEach((subSelect) => {
+    subSelect.style.maxHeight = null;
+    subSelect.style.paddingTop = 0;
+    subSelect.style.paddingBottom = 0;
+  });
+
+  subSelectBtn.forEach((btn) => {
+    if (btn.classList.contains('active')) {
+      btn.classList.remove('active');
+    }
+  });
+
   tabBtns.forEach((btn) => {
     if (!btn.getAttribute('disabled')) {
       btn.setAttribute('disabled', true);
@@ -55,7 +106,7 @@ selectBtn.addEventListener('click', () => {
     select.style.paddingTop = 0;
     select.style.paddingBottom = 0;
   } else {
-    select.style.height = select.scrollHeight + 20 + 'px';
+    // select.style.height = select.scrollHeight + 20 + 'px';
     select.style.maxHeight = select.scrollHeight + 20 + 'px';
     select.style.paddingBottom = 10 + 'px';
     select.style.paddingTop = 10 + 'px';
